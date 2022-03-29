e1aaaea08e6031cb1384ffd7bbbef06ae0c79ec5
Ubuntu <ubuntu@ip-172-31-70-57.ec2.internal>
2022-03-29 14:58:39 +0000

    fds

--- numGuess.cpp
+++ numGuess.cpp
@@ -4,29 +4,48 @@
 // Created on: March 29 2022
 // This program makes a random number game
 
-#include <iostream>
 #include <stdlib.h>
-#include <time.h>  
+#include <iostream>
+#include <random>
+
 using std::cout;
 using std::cin;
+using std::string;
 
+int main() {
+  int biggestNum, usersNum, randomNum;
+  cout << "Enter the largest number to be generated: ";
+  cin >> biggestNum;
+  std::random_device rseed;
+  std::mt19937 rgen(rseed());
+  std::uniform_int_distribution<int> idist(0, biggestNum);
+  randomNum = idist(rgen);
 
-int main() {  
-    int biggestNum, usersNum, randomNum;   
-    srand (time(NULL));
-    randomNum = rand() % biggestNum + 1;
-   
-    while (true){
-        cout << "Enter the largest number to be generated: ";
-        cin >> biggestNum;
-        cout << "Guess a number between 1 and " << biggestNum << ": ";
-        cin >> usersNum;
-        
-        if (randomNum<usersNum) puts ("The secret number is lower");
-        else if (randomNum>usersNum) puts ("The secret number is higher");
-        // else{
-        //     cout << "congrajulations"
-        // }
-    } 
+  while (true) {
+    cout << "Guess a number between 1 and " << biggestNum << ": ";
+    cin >> usersNum;
+
+    if (randomNum < usersNum) {
+        cout << "The secret number is lower\n";
+    } else if (randomNum > usersNum) {
+       cout << "The secret number is higher\n";
+    } else {
+      cout << "You guessed the right number!\n";
+      break;
+    }
+  }
+  string tryAgain;
+  cout << "would you like to try again? ";
+  cin >> tryAgain;
+  if (tryAgain == "y"
+    || tryAgain == "Y"
+    || tryAgain == "yes"
+    || tryAgain == "YES") {
+    main();
+  } else if (tryAgain == "n"
+    || tryAgain == "N"
+    || tryAgain == "no"
+    || tryAgain == "NO") {
+    exit(1);
+  }
 }
-
